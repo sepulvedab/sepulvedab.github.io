b9a5695926bcbcb54cda9281f9104aea8cb690c0
Carlos Sepúlveda <sepulveda-carlos@outlook.com>
2024-01-06 16:14:45 -0500

    basic design done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -67,3 +67,43 @@ buttonAbout.addEventListener("click", function() {
   
 });
 
+buttonProjects = document.getElementById("btn_Projects");
+buttonProjects.addEventListener("click", function() {
+  document.getElementById('content').innerHTML = `
+
+  <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-white pb-2 text-center">My Last Projects</h1>
+
+  <div class="flex flex-wrap -m-2">
+  <div class="p-2 w-full">
+    <div class="h-full flex items-center text-center border-gray-800  rounded-lg">
+      <div class="flex-grow">
+        <p class="text-gray-400">Coming soon.</p>
+      </div>
+    </div>
+  </div>
+  <div class="p-2  md:w-1/2  w-full">
+    <div class="h-full flex items-center border-gray-800 border p-1 rounded-lg">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-16 h-16 object-center flex-shrink-0  mr-4" viewBox="0 0 16 16">
+    <path d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353zM5.496 6.033a.237.237 0 0 1-.24-.247C5.35 4.091 6.737 3.5 8.005 3.5c1.396 0 2.672.73 2.672 2.24 0 1.08-.635 1.594-1.244 2.057-.737.559-1.01.768-1.01 1.486v.105a.25.25 0 0 1-.25.25h-.81a.25.25 0 0 1-.25-.246l-.004-.217c-.038-.927.495-1.498 1.168-1.987.59-.444.965-.736.965-1.371 0-.825-.628-1.168-1.314-1.168-.803 0-1.253.478-1.342 1.134-.018.137-.128.25-.266.25h-.825zm2.325 6.443c-.584 0-1.009-.394-1.009-.927 0-.552.425-.94 1.01-.94.609 0 1.028.388 1.028.94 0 .533-.42.927-1.029.927"/>
+  </svg>
+      <div class="flex-grow">
+        <h2 class="text-white title-font font-medium">Project name</h2>
+        <p class="text-gray-600">Some info.</p>
+      </div>
+    </div>
+  </div>
+  <div class="p-2 md:w-1/2 w-full">
+    <div class="h-full flex items-center border-gray-800 border p-1 rounded-lg">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-16 h-16 object-center flex-shrink-0  mr-4" viewBox="0 0 16 16">
+    <path d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353zM5.496 6.033a.237.237 0 0 1-.24-.247C5.35 4.091 6.737 3.5 8.005 3.5c1.396 0 2.672.73 2.672 2.24 0 1.08-.635 1.594-1.244 2.057-.737.559-1.01.768-1.01 1.486v.105a.25.25 0 0 1-.25.25h-.81a.25.25 0 0 1-.25-.246l-.004-.217c-.038-.927.495-1.498 1.168-1.987.59-.444.965-.736.965-1.371 0-.825-.628-1.168-1.314-1.168-.803 0-1.253.478-1.342 1.134-.018.137-.128.25-.266.25h-.825zm2.325 6.443c-.584 0-1.009-.394-1.009-.927 0-.552.425-.94 1.01-.94.609 0 1.028.388 1.028.94 0 .533-.42.927-1.029.927"/>
+  </svg>
+        <div class="flex-grow">
+        <h2 class="text-white title-font font-medium">Project name.</h2>
+        <p class="text-gray-600">Some info.</p>
+      </div>
+    </div>
+  </div>
+     
+  `;
+  
+});
