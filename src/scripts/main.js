90fc13a631ce742bcaaabad735ec7fb1b51305a6
Carlos Sepúlveda <sepulveda-carlos@outlook.com>
2024-01-06 09:31:21 -0500

    add main script

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+content = document.getElementById("content");
+
+buttonAbout = document.getElementById("btn_About");
+buttonAbout.addEventListener("click", function() {
+  document.getElementById('content').innerHTML = `
+
+
+  
+     
+  `;
+  
+});
+
