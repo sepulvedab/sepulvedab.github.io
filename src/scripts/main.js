b354ca3ebc5e0895cdf333149c29596aac735283
Carlos Sepúlveda <sepulveda-carlos@outlook.com>
2024-01-11 22:05:41 -0500

    first project added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -83,16 +83,16 @@ buttonProjects.addEventListener("click", function() {
   </div>
   
   <div class="p-2  md:w-1/2  w-full">
-    <a href="">
+    <a href="https://100dop.vercel.app/" target="_blank">
     
     
   <div class="h-full flex items-center border-gray-800 border p-1 rounded-lg">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-16 h-16 object-center flex-shrink-0  mr-4" viewBox="0 0 16 16">
-      <path d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353zM5.496 6.033a.237.237 0 0 1-.24-.247C5.35 4.091 6.737 3.5 8.005 3.5c1.396 0 2.672.73 2.672 2.24 0 1.08-.635 1.594-1.244 2.057-.737.559-1.01.768-1.01 1.486v.105a.25.25 0 0 1-.25.25h-.81a.25.25 0 0 1-.25-.246l-.004-.217c-.038-.927.495-1.498 1.168-1.987.59-.444.965-.736.965-1.371 0-.825-.628-1.168-1.314-1.168-.803 0-1.253.478-1.342 1.134-.018.137-.128.25-.266.25h-.825zm2.325 6.443c-.584 0-1.009-.394-1.009-.927 0-.552.425-.94 1.01-.94.609 0 1.028.388 1.028.94 0 .533-.42.927-1.029.927"/>
+      <path fill-rule="evenodd" d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zm-9.736 7.35v3.999h-.791v-1.714H1.79v1.714H1V11.85h.791v1.626h1.682V11.85h.79Zm2.251.662v3.337h-.794v-3.337H4.588v-.662h3.064v.662zm2.176 3.337v-2.66h.038l.952 2.159h.516l.946-2.16h.038v2.661h.715V11.85h-.8l-1.14 2.596H9.93L8.79 11.85h-.805v3.999zm4.71-.674h1.696v.674H12.61V11.85h.79v3.325Z"/>
     </svg>
       <div class="flex-grow">
-        <h2 class="text-white title-font font-medium">Project name</h2>
-        <p class="text-gray-600">Some info.</p>
+        <h2 class="text-white title-font font-medium">100 day of projects</h2>
+        <p class="text-gray-600">Practice for front-end skills.</p>
       </div>
     </div>
     </a>
